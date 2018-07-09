8515253ba80e949de13b557eef2126c420a9889b
Code4Cat <tongkaitik1997@gmail.com>
2018-07-09 20:58:38 +0800

    Add conn.php

---
+++ main/connection/conn.php
@@ -0,0 +1,7 @@
+<?php
+$hostname = "127.0.0.1";
+$database = "projectDB";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($hostname, $username, $password, $database) or die(mysqli_connect_error());
